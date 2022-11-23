44f9972856d25be30d90e51b6cb373364cbef4fe
martin <martin@ayesa.com>
2022-11-23 13:26:36 +0100

    Compila Modelo aparte

--- platform/proas-backend/src/main/java/com/apba/proas/backend/model/JSonStr.java
+++ platform/proas-backend/src/main/java/com/apba/proas/backend/model/JSonStr.java
@@ -1,23 +1,17 @@
 package com.apba.proas.backend.model;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 public class JSonStr {
-    static boolean DEBUG = true;
-
     static JSonStr jSonStr = new JSonStr();
-    Logger log;
     Gson gson;
 
     public JSonStr() {
-        log = LoggerFactory.getLogger(JSonStr.class);
-
         GsonBuilder gsb = new GsonBuilder();
+
         // Date: standard forms ("yyyy-MM-dd'T'HH:mm:ss.SSSX",
         // "yyyy-MM-dd'T'HH:mm:ss.SSS", "EEE, dd MMM yyyy HH:mm:ss zzz", "yyyy-MM-dd"))
         gsb.setDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -40,8 +34,6 @@ public class JSonStr {
 
             JsonElement je = JsonParser.parseString(jsonOutput);
             String prettyJsonString = gson.toJson(je);
-            if (DEBUG)
-                log.debug(String.format("------------%s: %s", o.getClass().getName(), prettyJsonString));
             return prettyJsonString;
         }
     }
